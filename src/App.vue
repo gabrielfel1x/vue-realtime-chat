0a37aa2adf93a6b56ddc4a49697dec85352ee073
gabrielfel1x <gabrielfelxx@gmail.com>
2024-05-30 23:09:35 -0300

    fix bugs in app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
 import { reactive, onMounted } from "vue";
-import { useRouter, useRoute } from "vue-router";
+// @ts-ignore
 import db from "./db";
 
 const state = reactive({
@@ -14,43 +14,6 @@ const state = reactive({
   group: "",
   messages: [] as { id: string; username: string; content: string }[],
 });
-
-function handleLogin(username: string) {
-  state.username = username;
-}
-
-function handleGroupSelect(group: string) {
-  state.group = group;
-  loadMessages(group);
-}
-
-function handleLogout() {
-  state.username = "";
-  state.group = "";
-  state.messages = [];
-}
-
-function handleClearMessages() {
-  if (state.group) {
-    db.ref(`messages/${state.group}`)
-      .remove()
-      .then(() => {
-        state.messages = [];
-      })
-      .catch((error: any) => {
-        console.error("Error removing messages: ", error);
-      });
-  }
-}
-
-function handleSendMessage(content: string) {
-  const message = {
-    id: Date.now().toString(),
-    username: state.username,
-    content,
-  };
-  db.ref(`messages/${state.group}`).push(message);
-}
 
 function loadMessages(group: string) {
   const messagesRef = db.ref(`messages/${group}`);
